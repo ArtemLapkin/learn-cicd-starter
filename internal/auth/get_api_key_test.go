885064d5e2a46d8755049fbb491b25bc99e65442
Artem Lapkin <lapkin.artem@gmail.com>
2026-06-08 10:01:28 +0300

    feat: test with broken code

---
+++ internal/auth/get_api_key_test.go
@@ -0,0 +1,58 @@
+package auth
+
+import (
+	"errors"
+	"net/http"
+	"reflect"
+	"testing"
+)
+
+func TestGetAPIKey(t *testing.T) {
+
+	token := "mytoken123"
+
+	// Scenario 1: Success
+	t.Run("Successfully get API key", func(t *testing.T) {
+		headers := http.Header{}
+		headers.Set("Authorization", "ApiKey "+token)
+		got, err := GetAPIKey(headers)
+
+		if err != nil {
+			t.Fatalf("unexpected error: %v", err)
+		}
+
+		want := token
+		if !reflect.DeepEqual(want, got) {
+			t.Fatalf("expected: %v, got: %v", want, got)
+		}
+	})
+
+	//t.Run("Wrong Auth header with more than 2 strings", func(t *testing.T) {
+	//	headers := http.Header{}
+	//	headers.Set("Authorization", "ApiKey mytoken123 and something else")
+	//	_, err := GetAPIKey(headers)
+	//	if err == nil {
+	//		t.Fatal("Supposed to get an error during getting API key with wrong header, but there is no error")
+	//	}
+	//})
+
+	//t.Run("Wrong Auth header with empty key", func(t *testing.T) {
+	//	headers := http.Header{}
+	//	headers.Set("Authorization", "ApiKey ")
+	//
+	//	_, err := GetAPIKey(headers)
+	//	if err == nil {
+	//		t.Fatal("Supposed to get an error during getting API key, its empty")
+	//	}
+	//})
+
+	t.Run("Empty Auth header", func(t *testing.T) {
+		headers := http.Header1{}
+
+		_, err := GetAPIKey(headers)
+		if !errors.Is(err, ErrNoAuthHeaderIncluded) {
+			t.Fatal("wrong error message")
+		}
+	})
+
+}
